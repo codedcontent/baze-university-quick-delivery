9d5ffee7f36c9a313627c6200a01459a4ac427a5
codedcontent <ogemephors002@gmail.com>
2023-02-19 19:08:19 +0100

    Changed the banner text

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -4,11 +4,15 @@ const Banner = () => {
   return (
     <div className="h-16 flex w-full items-center xl:px-20 lg:px-16 md:px-12 px-5 border-b-[1px] fixed top-0 left-0 z-10 bg-white">
       <p className="font-poppins w-max md:text-base text-xs">
-        <span className="text-secondary underline font-bold">
+        {/* <span className="text-secondary underline font-bold">
           Refer a friend
         </span>{" "}
         and get <span className="text-primary">10% </span> discount on your next
-        meal
+        meal */}
+        <span className="text-accent font-medium">
+          Ensure you provide accurate information when ordering help make your
+          delivery easier.
+        </span>
       </p>
     </div>
   );
